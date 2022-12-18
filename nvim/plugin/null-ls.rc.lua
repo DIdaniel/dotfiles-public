8a1870a958f88f5f1ce453bf26339ef97c475b9a
DONG IL <cappu159@gmail.com>
2022-12-18 20:39:31 +0900

    fish, nvim, tmux updated

---
+++ nvim/plugin/null-ls.rc.lua
@@ -0,0 +1,65 @@
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end
+-- local status2, eslint = pcall(require, "eslint")
+-- if (not status2) then return end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+    null_ls.builtins.diagnostics.fish
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end
+}
+
+-- eslint.setup {
+--   bin = 'eslint_d', -- or `eslint`
+--   code_actions = {
+--     enable = true,
+--     apply_on_save = {
+--       enable = true,
+--       types = { "directive", "problem", "suggestion", "layout" },
+--     },
+--     disable_rule_comment = {
+--       enable = true,
+--       location = "same_line", -- or `separate_line`
+--     },
+--   },
+--   diagnostics = {
+--     enable = true,
+--     report_unused_disable_directives = false,
+--     run_on = "type", -- or `save`
+--   }
+-- }
+
+vim.api.nvim_create_user_command(
+  'DisableLspFormatting',
+  function()
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+  end,
+  { nargs = 0 }
+)
